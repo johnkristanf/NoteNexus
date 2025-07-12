04a917665d2e725b6129d248727dbc932c0f2512
John Kristan Torremocha <johnkristan01@gmail.com>
2025-07-12 16:02:09 +0800

    post request send message

---
+++ lib/api/messages/post.ts
@@ -0,0 +1,48 @@
+import { Message, MessagePayload } from '@/types/message'
+
+export async function createNewMessage(message: Message) {
+    try {
+        const response = await fetch('http://localhost:8000/api/v1/new/message', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(message),
+        })
+
+        if (!response.ok) {
+            throw new Error(`Failed to create chat: ${response.status}`)
+        }
+
+        const data = await response.json()
+        return data.id
+    } catch (error) {
+        console.error('Error creating message:', error)
+        throw new Error('Unexpected error occured. Please try again')
+    }
+}
+
+
+
+export async function sendLLMMessage(payload: MessagePayload) {
+    try {
+        const response = await fetch('http://localhost:8000/api/v1/send/message', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload),
+        })
+
+        if (!response.ok) {
+            throw new Error(`Failed to create chat: ${response.status}`)
+        }
+
+        const data = await response.json()
+        return data.content;
+
+    } catch (error) {
+        console.error('Error sending chat:', error)
+        throw new Error('Unexpected error occured. Please try again')
+    }
+}
