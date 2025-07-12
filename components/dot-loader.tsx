005f0dc8a98fe7caf1672808b3083c4c086ba8a2
John Kristan Torremocha <johnkristan01@gmail.com>
2025-07-12 15:57:48 +0800

    dot loader animation on conversation

---
+++ components/dot-loader.tsx
@@ -0,0 +1,7 @@
+export const DotLoader = () => (
+    <div className="flex gap-1 items-center px-3 py-2">
+        <span className="h-2 w-2 bg-white rounded-full animate-bounce [animation-delay:-0.3s]" />
+        <span className="h-2 w-2 bg-white rounded-full animate-bounce [animation-delay:-0.15s]" />
+        <span className="h-2 w-2 bg-white rounded-full animate-bounce" />
+    </div>
+)
