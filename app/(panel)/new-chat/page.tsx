cbec186c29f1bdc2c0992ddcf6758d74672ada80
John Kristan Torremocha <johnkristan01@gmail.com>
2025-07-12 16:00:34 +0800

    redirect to dynamic chat page after input

--- app/(panel)/new-chat/page.tsx
+++ app/(panel)/new-chat/page.tsx
@@ -1,62 +1,40 @@
 'use client'
 
-import { startChatStream } from '@/lib/event-source'
-import { Link, Mic, Send, Square } from 'lucide-react'
-import { useRef, useState } from 'react'
+import { createNewChat } from '@/lib/api/chats/post'
+import { useChatStore } from '@/store/chatStore'
+import { Link, Mic, Send } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { toast } from 'sonner'
 
 export default function NewChatPage() {
     const [input, setInput] = useState('')
-    const [messages, setMessages] = useState<string[]>([])
-    const [isStreaming, setIsStreaming] = useState(false)
-    const controllerRef = useRef<EventSource | null>(null)
+    const router = useRouter()
 
-    const sessionId = 'user123'
-
-    const handleSendChat = () => {
-        if (!input.trim()) return
-        setMessages((prev) => [...prev, `🧑: ${input}`, `🤖: `])
-        setIsStreaming(true)
-
-        let streamedText = ''
-
-        controllerRef.current = startChatStream(input, sessionId, (chunk) => {
-            streamedText += chunk
-            setMessages((prev) =>
-                prev.map((msg, i) => (i === prev.length - 1 ? `🤖: ${streamedText}` : msg))
-            )
-            setIsStreaming(false)
-            setInput('')
-        })
-    }
-
-    const handleStop = () => {
-        if (controllerRef.current) {
-            controllerRef.current.close()
-            setIsStreaming(false)
+    const handleCreateNewChat = async () => {
+        try {
+            const chatID = await createNewChat()
+            if (chatID) {
+                useChatStore.getState().setInitialInput(input.trim())
+                router.push(`/chat/${chatID}`)
+            }
+        } catch (error) {
+            if (error instanceof Error) {
+                toast.error(error.message)
+            }
         }
     }
 
     return (
         <div className="h-[80vh] flex items-center justify-center ">
             <div className="w-full flex flex-col gap-5">
-                {messages.length <= 0 ? (
-                    // NOTE NEXUS INTRODUCTION
-                    <div className="flex flex-col items-center gap-1">
-                        <h1 className="font-bold text-3xl">
-                            Hi, I am Note <span className="text-violet-600">Nexus.</span>
-                        </h1>
-                        <p className="text-lg text-gray-500">How can I help in your studies?</p>
-                    </div>
-                ) : (
-                    // CONVERSATION BOX
-                    <div className="border rounded p-4 h-[300px] overflow-y-auto space-y-2 bg-gray-100 text-sm">
-                        {messages.map((msg, idx) => (
-                            <div key={idx} className="whitespace-pre-wrap">
-                                {msg}
-                            </div>
-                        ))}
-                    </div>
-                )}
+                {/* NOTE NEXUS INTRODUCTION */}
+                <div className="flex flex-col items-center gap-1">
+                    <h1 className="font-bold text-3xl">
+                        Hi, I am Note <span className="text-violet-600">Nexus.</span>
+                    </h1>
+                    <p className="text-lg text-gray-500">How can I help in your studies?</p>
+                </div>
 
                 {/* CHATBOX */}
                 <div className="w-full p-2 border-t border-slate-700 bg-slate-800 rounded-2xl">
@@ -66,46 +44,29 @@ export default function NewChatPage() {
                             placeholder="Send a message..."
                             value={input}
                             onChange={(e) => setInput(e.target.value)}
-                            disabled={isStreaming}
                             onKeyDown={(e) => {
-                                if (e.key === 'Enter') handleSendChat()
+                                if (e.key === 'Enter') handleCreateNewChat()
                             }}
-                            className="flex-1 bg-slate-800 text-white p-3 rounded-lg focus:outline-none "
+                            className="flex-1 bg-slate-800 text-white p-3 rounded-lg focus:outline-none"
                         />
                     </div>
 
                     <div className="flex justify-between">
-                        <button
-                            disabled={isStreaming}
-                            className="hover:opacity-75 hover:cursor-pointer text-white px-4 py-2 rounded-lg "
-                        >
+                        <button className="hover:opacity-75 hover:cursor-pointer text-white px-4 py-2 rounded-lg ">
                             <Link />
                         </button>
 
                         <div className="flex gap-2">
-                            <button
-                                disabled={isStreaming}
-                                className="hover:opacity-75 hover:cursor-pointer text-white px-4 py-2 rounded-lg "
-                            >
+                            <button className="hover:opacity-75 hover:cursor-pointer text-white px-4 py-2 rounded-lg ">
                                 <Mic />
                             </button>
 
-                            {!isStreaming ? (
-                                <button
-                                    onClick={handleSendChat}
-                                    className="hover:opacity-75 hover:cursor-pointer bg-violet-600 text-white px-4 py-2 rounded"
-                                    disabled={isStreaming}
-                                >
-                                    <Send />
-                                </button>
-                            ) : (
-                                <button
-                                    onClick={handleStop}
-                                    className="hover:opacity-75 hover:cursor-pointer text-red-500 underline"
-                                >
-                                    <Square />
-                                </button>
-                            )}
+                            <button
+                                onClick={handleCreateNewChat}
+                                className="hover:opacity-75 hover:cursor-pointer bg-violet-600 text-white px-4 py-2 rounded"
+                            >
+                                <Send />
+                            </button>
                         </div>
                     </div>
                 </div>
