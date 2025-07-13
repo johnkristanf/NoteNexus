3d110838e070aea44a39500c4086ea105e2e3986
John Kristan Torremocha <johnkristan01@gmail.com>
2025-07-14 04:26:44 +0800

    state store for learning material both file and name

--- store/chatStore.ts
+++ store/chatStore.ts
@@ -3,11 +3,31 @@ import { create } from 'zustand'
 type ChatStore = {
     initialInput: string
     setInitialInput: (input: string) => void
-    clear: () => void
+    clearInitialInput: () => void
+
+    learningMaterialName: string
+    uploadedLearningMaterialFile: File | null
+    setUploadedLearningMaterialFile: (file: File | null) => void
+    setLearningMaterialName: (name: string) => void
+    
+    clearlearningMaterialName: () => void
+    clearUploadedLearningMaterialFile: () => void
+
 }
 
 export const useChatStore = create<ChatStore>((set) => ({
     initialInput: '',
     setInitialInput: (input) => set({ initialInput: input }),
-    clear: () => set({ initialInput: '' }),
+    clearInitialInput: () => set({ initialInput: '' }),
+
+    
+
+    learningMaterialName: '',
+    uploadedLearningMaterialFile: null,
+    setUploadedLearningMaterialFile: (file) => set({ uploadedLearningMaterialFile: file }),
+    setLearningMaterialName: (name) => set({ learningMaterialName: name }),
+
+    clearlearningMaterialName: () => set({ learningMaterialName: '' }),
+    clearUploadedLearningMaterialFile: () => set({ uploadedLearningMaterialFile: null }),
+
 }))
