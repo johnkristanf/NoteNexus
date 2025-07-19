cc45bb54ca48b5f90f3ab855c70cb7a697c1eb36
John Kristan Torremocha <johnkristan01@gmail.com>
2025-07-19 14:43:28 +0800

    redeploy

--- database/index.ts
+++ database/index.ts
@@ -6,7 +6,7 @@ const client = postgres(process.env.DATABASE_URL!)
 // Pass the client to drizzle
 export const db = drizzle(client)
 
-// ONLY USE THIS FOR SEEDING
+// ONLY USE THIS FOR SEEDING 
 async function main() {}
 
 main()
